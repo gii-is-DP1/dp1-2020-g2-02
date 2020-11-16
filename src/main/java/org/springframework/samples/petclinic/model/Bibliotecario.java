10b3d4c107a94512729963c4d76c618844ef5584
rhrjo <rhrjo@jorge-pc>
2020-11-16 12:36:03 +0100

    Modificación de la entidad Bibliotecario y ahora se muestra el css en listBibliotecario

--- src/main/java/org/springframework/samples/petclinic/model/Bibliotecario.java
+++ src/main/java/org/springframework/samples/petclinic/model/Bibliotecario.java
@@ -3,21 +3,41 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
-import lombok.Data;
-
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 @Entity
-@Data
 @Table(name = "bibliotecarios")
 
 public class Bibliotecario extends BaseEntity {
-	
-		@Column(length=1024)     // Needed in some environments for strings longer than 255 characters
+
+		@Column(name = "nombre")
+		@NotEmpty
 		private String nombre;
+		
+		@Column(name = "apellidos")
+		@NotEmpty
 		private String apellidos;
+		
+		@Column(name = "dni")
+		@NotEmpty
+		@Pattern(regexp = "^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$")
 		private String dni;
+		
+		@Column(name = "telefono")
+		@NotEmpty
+		@Digits(fraction = 0, integer = 10)
 		private Integer telefono;
+		
+		@Column(name = "email")
+		@NotEmpty
+		@Email
 		private String email;
+		
+		@Column(name = "pass")
+		@NotEmpty
+		@Pattern(regexp ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$") //Contraseña con minúsculas, mayúsculas, números y entre 8 y 32 caracteres
 		private String pass;
 		
 }
