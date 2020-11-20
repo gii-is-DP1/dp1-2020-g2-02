7672dd135e7020a3d53f690a65b912aba7075142
Fernando González <fergongar1@alum.us.es>
2020-11-20 18:29:12 +0100

    Implementación de la entidad Novedad. Actualización de la entidad Bibliotecario, corrigiendo la vista de lista y añadiéndole la relación con la entidad User.

---
+++ src/main/java/org/springframework/samples/petclinic/service/NovedadService.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.service;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Novedad;
+import org.springframework.samples.petclinic.repository.NovedadRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NovedadService {
+	@Autowired
+	private NovedadRepository novedadRepo;
+	
+	@Transactional
+	public int novedadCount() {
+		return (int) novedadRepo.count();
+	}
+	
+	@Transactional
+	public Iterable<Novedad> findAll() {
+		return novedadRepo.findAll();
+	}
+
+	public void save(Novedad novedad) {
+		novedadRepo.save(novedad);
+	}
+
+}
