14a07f7ea4e6767f78545b5f4a06ac35d2e32e8d
veris744 <71214562+veris744@users.noreply.github.com>
2020-11-23 14:12:50 +0100

    Update PrestamoRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/PrestamoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PrestamoRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package org.springframework.samples.petclinic.repository;
 
 import java.util.Collection;
@@ -11,17 +11,3 @@ public interface PrestamoRepository extends CrudRepository<Prestamo,Integer>{
 		Collection<Prestamo> findAll();
 	
 }
-=======
-package org.springframework.samples.petclinic.repository;
-
-import java.util.Collection;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.petclinic.model.Prestamo;
-
-
-public interface PrestamoRepository extends CrudRepository<Prestamo,Integer>{
-		Collection<Prestamo> findAll();
-	
-}
->>>>>>> parent of af356ce... Añadido autores
