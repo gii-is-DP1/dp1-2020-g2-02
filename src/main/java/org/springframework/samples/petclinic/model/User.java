46e9257da8213dcccd3af60150913ae0fdff28f7
fergongar1 <73081243+fergongar1@users.noreply.github.com>
2020-11-23 13:30:08 +0100

    Revert "Actualización rama jorruirui1"

--- src/main/java/org/springframework/samples/petclinic/model/User.java
+++ src/main/java/org/springframework/samples/petclinic/model/User.java
@@ -3,10 +3,13 @@ package org.springframework.samples.petclinic.model;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import lombok.Data;
 
@@ -17,6 +20,9 @@ public class User{
 	@Id
 	String username;
 	
+	@Column(name = "pass")
+	@NotEmpty
+	@Pattern(regexp ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$") //Contraseña con minúsculas, mayúsculas, números y entre 8 y 32 caracteres
 	String password;
 	
 	boolean enabled;
