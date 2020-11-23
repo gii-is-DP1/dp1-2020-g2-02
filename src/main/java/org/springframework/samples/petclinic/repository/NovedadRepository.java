77ec7d71eb00e670b1d748da9398f12a7c0cfc6f
veris744 <71214562+veris744@users.noreply.github.com>
2020-11-23 14:12:27 +0100

    Update NovedadRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/NovedadRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/NovedadRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package org.springframework.samples.petclinic.repository;
 
 import java.util.Collection;
@@ -9,15 +9,3 @@ import org.springframework.samples.petclinic.model.Novedad;
 public interface NovedadRepository extends CrudRepository<Novedad,Integer>{
 	Collection<Novedad> findAll();
 }
-=======
-package org.springframework.samples.petclinic.repository;
-
-import java.util.Collection;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.petclinic.model.Novedad;
-
-public interface NovedadRepository extends CrudRepository<Novedad,Integer>{
-	Collection<Novedad> findAll();
-}
->>>>>>> parent of af356ce... Añadido autores
