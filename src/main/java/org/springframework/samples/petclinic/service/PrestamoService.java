1fe73f7c6d8fbdd0dde8f82d4f44c616eec506b6
fergongar1 <73081243+fergongar1@users.noreply.github.com>
2020-11-23 14:11:34 +0100

    Update PrestamoService.java

--- src/main/java/org/springframework/samples/petclinic/service/PrestamoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/PrestamoService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.springframework.samples.petclinic.service;
 
 import java.util.Collection;
@@ -33,39 +32,3 @@ public class PrestamoService {
 	
 	
 }
-=======
-package org.springframework.samples.petclinic.service;
-
-import java.util.Collection;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Prestamo;
-import org.springframework.samples.petclinic.repository.PrestamoRepository;
-
-public class PrestamoService {
-
-	@Autowired
-	PrestamoRepository PrestamoRepo;
-	
-	public Collection<Prestamo> findAll(){
-		return PrestamoRepo.findAll();
-	}
-	
-	public Optional<Prestamo> findById(int id){
-		return PrestamoRepo.findById(id);
-	}
-	
-	public void delete(Prestamo Prestamo) {
-		PrestamoRepo.deleteById(Prestamo.getId());
-	}
-	
-	public void save (@Valid Prestamo Prestamo) {
-		PrestamoRepo.save(Prestamo);
-	}
-	
-	
-}
->>>>>>> parent of af356ce... Añadido autores
