db3024f81455b14d060172128972013369e749f5
Isabel <isadede@alum.us.es>
2020-12-09 16:09:17 +0100

    AutorService y LibroService tests actualizados

--- src/test/java/org/springframework/samples/petclinic/service/LibroServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/LibroServiceTest.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.service;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
 import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.samples.petclinic.model.Autor;
 import org.springframework.samples.petclinic.model.Genero;
 import org.springframework.samples.petclinic.model.Libro;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class LibroServiceTest {
@@ -42,5 +44,19 @@ public class LibroServiceTest {
 		Libro libro = libroService.findById(1).get();
 		assertTrue(libro.getTitulo().equals("El adversario")); 
 	}
+	
+	@Test
+	@Transactional
+	public void testAddLibro() {
+		Libro libro = new Libro();
+		libro.setISBN("1234567890");
+		libro.setIdioma("Inglés");
+		libro.setTitulo("TítuloPrueba");
+		libro.setFecha_publicacion(LocalDate.now());
+		libroService.save(libro);
+		Collection<Libro> libros = libroService.findAll();
+		assertTrue(libros.size()==3);
+	}
+	
 	
 }
