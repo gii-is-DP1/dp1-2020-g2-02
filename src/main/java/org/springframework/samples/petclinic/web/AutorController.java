0b325f2b4dfbdcca0c19602758a58783e5086f70
Isabel <isadede@alum.us.es>
2020-12-11 13:36:52 +0100

    Actualización librosAutor y AutorController

--- src/main/java/org/springframework/samples/petclinic/web/AutorController.java
+++ src/main/java/org/springframework/samples/petclinic/web/AutorController.java
@@ -2,12 +2,17 @@ package org.springframework.samples.petclinic.web;
 
 import java.security.Principal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Autor;
+import org.springframework.samples.petclinic.model.Genero;
+import org.springframework.samples.petclinic.model.Libro;
 import org.springframework.samples.petclinic.service.AutorService;
+import org.springframework.samples.petclinic.service.LibroService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -23,6 +28,9 @@ public class AutorController {
 
 	@Autowired
 	private AutorService autorService;
+
+	@Autowired
+	private LibroService libroService;
 	
 	@GetMapping
 	public String listAutores(ModelMap model) {
@@ -57,7 +65,16 @@ public class AutorController {
 	public ModelAndView verLibrosAutor(@PathVariable("autorId") int autorId) {
 		ModelAndView mav = new ModelAndView("autores/librosAutor");
 		mav.addObject("autor", this.autorService.findById(autorId).get());
-		mav.addObject("libros", this.autorService.getLibrosAutor(autorService.findById(autorId).get()));
+		
+		
+		Iterator<Libro> libros = this.autorService.getLibrosAutor(autorService.findById(autorId).get()).iterator();
+		HashMap <Libro,Collection<Genero>> generosLibros = new HashMap<>();
+		while(libros.hasNext()) {
+			Libro libro = libros.next();
+			generosLibros.put(libro, libroService.getGenerosLibro(libro));
+		}
+		
+		mav.addObject("libros", generosLibros);
 		return mav;
 	}
 }
