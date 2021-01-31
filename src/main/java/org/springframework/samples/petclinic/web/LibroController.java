14eb75068900b91fdc03c625a88eea61c848544c
Fernando González <fergongar1@alum.us.es>
2021-01-31 15:08:42 +0100

    Implementada reserva de préstamos mediante excepciones. Añadidos tests.

--- src/main/java/org/springframework/samples/petclinic/web/LibroController.java
+++ src/main/java/org/springframework/samples/petclinic/web/LibroController.java
@@ -1,26 +1,25 @@
 package org.springframework.samples.petclinic.web;
 
 import java.security.Principal;
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Disponibilidad;
-import org.springframework.samples.petclinic.model.Ejemplar;
 import org.springframework.samples.petclinic.model.Libro;
 import org.springframework.samples.petclinic.model.Miembro;
-import org.springframework.samples.petclinic.model.Prestamo;
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.service.EjemplarService;
 import org.springframework.samples.petclinic.service.LibroService;
 import org.springframework.samples.petclinic.service.MiembroService;
 import org.springframework.samples.petclinic.service.PrestamoService;
 import org.springframework.samples.petclinic.service.UserService;
+import org.springframework.samples.petclinic.service.exceptions.LibroNoDisponibleException;
+import org.springframework.samples.petclinic.service.exceptions.LibroNoExistenteException;
+import org.springframework.samples.petclinic.service.exceptions.LibroYaEnPrestamoException;
+import org.springframework.samples.petclinic.service.exceptions.PrestamoConRetrasoException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -54,12 +53,18 @@ public class LibroController {
 	public String listLibros(ModelMap model, @RequestParam(required = false) String q, @RequestParam(required = false) String qAutor, @RequestParam(required = false) String qEditorial) {
 		String vista = "libros/listLibro";
 		Collection<Libro> libros = librosService.findAll();
+		
 		//Halla si hay algún ejemplar disponible para cada libro.
 		Map<Integer, Boolean> disponibilidad = libros.stream().collect(Collectors.toMap(x->x.getId(), y->!ejemplarService.findDisponibles(y).isEmpty()));
-		//Filtra por nombre en caso de que haya una búsqueda.
-		if(q != null && !q.isEmpty()) libros=libros.stream().filter(x->x.getTitulo().toLowerCase().contains(q.toLowerCase())).collect(Collectors.toList());
-		if(qAutor != null && !qAutor.isEmpty()) libros=libros.stream().filter(x->x.getAutores().stream().anyMatch(y->y.getNombre().toLowerCase().contains(qAutor.toLowerCase()))).collect(Collectors.toList());
-		if(qEditorial != null && !qEditorial.isEmpty()) libros=libros.stream().filter(x->x.getEditorial().getNombre().toLowerCase().contains(q.toLowerCase())).collect(Collectors.toList());
+		
+		//Filtra y ordena el resultado.
+		if(q != null && !q.isEmpty()) 
+			libros=libros.stream().filter(x->x.getTitulo().toLowerCase().contains(q.toLowerCase())).collect(Collectors.toList());
+		if(qAutor != null && !qAutor.isEmpty()) 
+			libros=libros.stream().filter(x->x.getAutores().stream().anyMatch(y->y.getNombre().toLowerCase().contains(qAutor.toLowerCase()))).collect(Collectors.toList());
+		if(qEditorial != null && !qEditorial.isEmpty()) 
+			libros=libros.stream().filter(x->x.getEditorial().getNombre().toLowerCase().contains(q.toLowerCase())).collect(Collectors.toList());
+		
 		model.addAttribute("libros",libros);
 		model.addAttribute("disponibilidad", disponibilidad);
 		
@@ -68,42 +73,27 @@ public class LibroController {
 	
 	@GetMapping(path="/reservar/{libroId}")
 	public String reservar(@PathVariable("libroId") int libroId, ModelMap model, Principal principal) {
-		String vista = listLibros(model,null,null,null);
-		
-		//Comprueba si el libro existe
-		Optional<Libro> libro = librosService.findById(libroId);
-		if(!libro.isPresent()) {
-			model.addAttribute("message","Libro no existente");
-			return vista;
-		}
-		
-		//Comprueba si el usuario tiene ya en préstamo ese libro.
 		User user = userService.findByUsername(principal.getName());
 		Miembro miembro = miembroService.findByUser(user);
-		Optional<Prestamo> prestamoExistente = prestamoService.prestamosDeLibroEnProceso(miembro, libro.get());
-		if(prestamoExistente.isPresent()) {
-			model.addAttribute("message","Ya tienes ese libro en préstamo");
-			return vista;
-		}
 		
-		//Comprueba si el libro no tiene ejemplares disponibles
-		Collection<Ejemplar> ejemplaresDisponibles = ejemplarService.findDisponibles(libro.get());
-		if(ejemplaresDisponibles.isEmpty()) {
-			model.addAttribute("message","Libro no disponible");
-		}
-		else {
-			Ejemplar ejemplar = ejemplaresDisponibles.iterator().next();
-			ejemplar.setDisponibilidad(Disponibilidad.RESERVADO);
-			Prestamo prestamo = new Prestamo();
-			prestamo.setFechaPrestamo(LocalDate.now());
-			prestamo.setFechaDevolucion(LocalDate.now().plusDays(16));
-			prestamo.setFinalizado(false);
-			prestamo.setEjemplar(ejemplar);
-			prestamo.setMiembro(miembro);
-
-			prestamoService.save(prestamo);
+		try {
+			prestamoService.realizarReserva(libroId, miembro);
 			model.addAttribute("message","Libro reservado, acuda a la biblioteca a recogerlo.");
 		}
+		catch(LibroNoExistenteException e) {
+			model.addAttribute("message","Libro no existente");
+		}
+		catch(LibroYaEnPrestamoException e) {
+			model.addAttribute("message","Ya tienes ese libro en préstamo");
+		}
+		catch(LibroNoDisponibleException e) {
+			model.addAttribute("message","Libro no disponible");
+		}
+		catch(PrestamoConRetrasoException e) {
+			model.addAttribute("message","Por favor, entregue su préstamo pendiente de devolución antes de realizar uno nuevo");
+		}
+		
+		String vista = listLibros(model,null,null,null);
 		return vista;
 	}
 	
