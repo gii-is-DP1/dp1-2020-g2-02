4d2e70307abaacee75c51ac168f196391895c3e3
fergongar1 <73081243+fergongar1@users.noreply.github.com>
2021-02-01 13:40:40 +0100

    Revert "Ac2"

--- src/main/java/org/springframework/samples/petclinic/model/Genero.java
+++ src/main/java/org/springframework/samples/petclinic/model/Genero.java
@@ -1,13 +1,9 @@
 package org.springframework.samples.petclinic.model;
 
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -29,13 +25,5 @@ public class Genero extends BaseEntity {
 	@Column(name = "genero")
 	@NotEmpty    
 	private String genero;
-	
-	@ManyToMany
-    @JoinTable(
-              name = "pertenece_a", 
-              joinColumns = @JoinColumn(name = "genero_id"), 
-              inverseJoinColumns = @JoinColumn(name = "libro_id"))     
-    @JoinColumn(name = "libro_id")
-    private List<Libro> libros;
 
 }
