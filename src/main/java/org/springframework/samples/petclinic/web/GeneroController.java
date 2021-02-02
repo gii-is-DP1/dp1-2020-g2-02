ba969650dfb41fd49ebb5a23dcacff9f297074b7
Beatriz Llamas <beallasai@alum.us.es>
2021-02-02 18:18:42 +0100

    editGenero

--- src/main/java/org/springframework/samples/petclinic/web/GeneroController.java
+++ src/main/java/org/springframework/samples/petclinic/web/GeneroController.java
@@ -1,14 +1,18 @@
 package org.springframework.samples.petclinic.web;
 
+import java.security.Principal;
 import java.util.Collection;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Genero;
 import org.springframework.samples.petclinic.service.GeneroService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,5 +30,28 @@ public class GeneroController {
 		model.addAttribute("generos", generos);
 		return vista;
 	}
+	
+	
+	@PostMapping(path="/save")
+	public String guardarGenero(@Valid Genero genero, BindingResult result, ModelMap modelmap, Principal principal) {
+		String vista = "generos/listGenero";
+		if(result.hasErrors()) {
+			modelmap.addAttribute("genero", genero);
+			modelmap.addAttribute("message", "Hay fallos en el formulario.");
+			return "generos/editLibro";
+		}else {
+			generoService.save(genero);
+			modelmap.addAttribute("message", "Género guardado correctamente");
+			vista = listGeneros(modelmap);
+		}
+		return vista;
+	}
+	
+	@GetMapping(path="/new")
+	public String crearGenero(ModelMap modelmap) {
+		String vista = "generos/editGenero";
+		modelmap.addAttribute("genero", new Genero());
+		return vista;
+	}	
 
 }
