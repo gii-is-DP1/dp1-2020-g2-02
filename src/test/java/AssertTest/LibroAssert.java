2b4309ad2faa72e0473a8e23f4cd3b410858f45f
Ivan Romero Costa <ivanromerocosta@gmail.com>
2021-02-05 19:26:13 +0100

    AssertJ implementacion

---
+++ src/test/java/AssertTest/LibroAssert.java
@@ -0,0 +1,25 @@
+package AssertTest;
+
+import org.assertj.core.api.AbstractAssert;
+import org.springframework.samples.petclinic.model.Libro;
+
+public class LibroAssert extends AbstractAssert<LibroAssert, Libro>{
+	 
+	public LibroAssert(Libro actual) {
+	        super(actual, LibroAssert.class);
+	    }
+	
+	public static LibroAssert assertThat(Libro actual) {
+	    return new LibroAssert(actual);
+	}
+	
+	public LibroAssert hasFullName(String fullName) {
+	    isNotNull();
+	    if (!actual.getTitulo().equals(fullName)) {
+	        failWithMessage("Expected person to have full name %s but was %s", 
+	          fullName, actual.getTitulo());
+	    }
+	    return this;
+	}
+
+}
