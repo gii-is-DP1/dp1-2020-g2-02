edebd8c5314804a0a70b14a4264b8052ca6c2e75
Fernando González <fergongar1@alum.us.es>
2021-02-09 16:39:55 +0100

    Tests WelcomeController

--- src/test/java/org/springframework/samples/petclinic/web/WelcomeControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/WelcomeControllerTest.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.web;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -8,10 +9,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
-import org.hibernate.mapping.Collection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,11 +21,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.model.Bibliotecario;
+import org.springframework.samples.petclinic.model.Authorities;
 import org.springframework.samples.petclinic.model.DatosDiarios;
-import org.springframework.samples.petclinic.model.Disponibilidad;
-import org.springframework.samples.petclinic.model.Ejemplar;
-import org.springframework.samples.petclinic.model.Libro;
+import org.springframework.samples.petclinic.model.Encargo;
 import org.springframework.samples.petclinic.model.Miembro;
 import org.springframework.samples.petclinic.model.Prestamo;
 import org.springframework.samples.petclinic.model.User;
@@ -68,30 +67,45 @@ public class WelcomeControllerTest {
 	void setup() {
 	
 		User usuario = new User();
-		usuario.setUsername("alecasgar");
-		usuario.setPassword("Ale123456");
+		usuario.setUsername("usuarioMiembro");
 		usuario.setEnabled(true);
 	
+		Set<Authorities> autoridades = new HashSet<>();
+		Authorities autoridad = new Authorities();
+		autoridad.setId(1);
+		autoridad.setAuthority("miembro");
+		autoridad.setUser(usuario);
+		autoridades.add(autoridad);
+		usuario.setAuthorities(autoridades);
 		
 		Miembro miembro = new Miembro();
 		miembro.setId(1);
-		miembro.setNombre("Alejandro");
-		miembro.setApellidos("Castro Garcia");
-		miembro.setDni("49586958D");
-		miembro.setTelefono("123456789");
-		miembro.setEmail("alecagar@gmail.com");
 		miembro.setUser(usuario);
+		
+		User usuario2 = new User();
+		usuario2.setUsername("usuarioBibliotecario");
+		usuario2.setEnabled(true);
+	
+		Set<Authorities> autoridades2 = new HashSet<>();
+		Authorities autoridad2 = new Authorities();
+		autoridad2.setId(2);
+		autoridad2.setAuthority("bibliotecario");
+		autoridad2.setUser(usuario2);
+		autoridades2.add(autoridad2);
+		usuario2.setAuthorities(autoridades2);
 		
 		DatosDiarios datosDiarios = new DatosDiarios();
 		datosDiarios.setFecha(LocalDate.now());
 		datosDiarios.setId(1);
-		DatosDiarios datosDiarios2 = new DatosDiarios();
-		datosDiarios2.setFecha(LocalDate.of(2020, 01, 01));
-		datosDiarios2.setId(2);
 		List<DatosDiarios> dd = new ArrayList<>();
-		dd.add(datosDiarios);dd.add(datosDiarios2);
+		dd.add(datosDiarios);
 		
 		given(this.datosService.findAllOrderByFecha()).willReturn(dd);
+		
+		given(this.userService.findByUsername("usuarioMiembro")).willReturn(usuario);
+		given(this.miembroService.findByUser(usuario)).willReturn(miembro);
+		
+		given(this.userService.findByUsername("usuarioBibliotecario")).willReturn(usuario2);
 		
 	}
 	
@@ -104,6 +118,58 @@ public class WelcomeControllerTest {
 			.andExpect(model().attributeExists("persons"))
 			.andExpect(model().attributeExists("title"))
 			.andExpect(model().attributeExists("group"))
+			.andExpect(view().name("welcome"));
+	}
+	
+	@WithMockUser(value = "usuarioMiembro")
+	@Test
+	void testInicioMiembro() throws Exception {
+		mockMvc.perform(get("/inicio"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeDoesNotExist("prestamoUrgente"))
+			.andExpect(model().attributeDoesNotExist("pedidoUrgente"))
+			.andExpect(model().attribute("message", "Bienvenido, usuarioMiembro"))
+			.andExpect(view().name("welcome"));
+	}
+	
+	@WithMockUser(value = "usuarioMiembro")
+	@Test
+	void testInicioMiembroPrestamoUrgente() throws Exception {
+		List<Prestamo> prestamos = new ArrayList<>();
+		Prestamo p = new Prestamo();
+		prestamos.add(p);
+		given(prestamoService.prestamosMiembrosUrgentes(any(Miembro.class))).willReturn(prestamos);
+		mockMvc.perform(get("/inicio"))
+			.andExpect(status().isOk())
+			.andExpect(model().attribute("prestamoUrgente", true))
+			.andExpect(model().attributeDoesNotExist("pedidoUrgente"))
+			.andExpect(model().attribute("message", "Bienvenido, usuarioMiembro"))
+			.andExpect(view().name("welcome"));
+	}
+	
+	@WithMockUser(value = "usuarioBibliotecario")
+	@Test
+	void testInicioBibliotecario() throws Exception {
+		mockMvc.perform(get("/inicio"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeDoesNotExist("prestamoUrgente"))
+			.andExpect(model().attributeDoesNotExist("pedidoUrgente"))
+			.andExpect(model().attribute("message", "Bienvenido, usuarioBibliotecario"))
+			.andExpect(view().name("welcome"));
+	}
+	
+	@WithMockUser(value = "usuarioBibliotecario")
+	@Test
+	void testInicioBibliotecarioEncargoUrgente() throws Exception {
+		List<Encargo> encargos = new ArrayList<>();
+		Encargo e = new Encargo();
+		encargos.add(e);
+		given(encargoService.pedidosUrgentes()).willReturn(encargos);
+		mockMvc.perform(get("/inicio"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeDoesNotExist("prestamoUrgente"))
+			.andExpect(model().attribute("pedidoUrgente", true))
+			.andExpect(model().attribute("message", "Bienvenido, usuarioBibliotecario"))
 			.andExpect(view().name("welcome"));
 	}
 }
